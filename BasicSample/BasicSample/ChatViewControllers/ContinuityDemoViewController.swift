cda9185d5d96117158c7125efcb57f8d1d00306c
Omer Rahmany <omer.rahmany@logmein.com>
2019-09-15 13:51:44 +0300

    [BLD-14697] Adding sample usage of eventTracking

--- BasicSample/BasicSample/ChatViewControllers/ContinuityDemoViewController.swift
+++ BasicSample/BasicSample/ChatViewControllers/ContinuityDemoViewController.swift
@@ -12,7 +12,39 @@ import Bold360AI
 class ContinuityDemoViewController: AgentViewController {
 
     override func viewDidLoad() {
+        // Do any additional setup after loading the view.
         super.viewDidLoad()
-    
+        self.view.backgroundColor = .white
+        let baseTrackParams = BaseTrackParams()
+
+        baseTrackParams.baseUri = URL(fileURLWithPath: "https://qa07.nanorep.com/~jio/api/analytics/v1/track/event")
+        baseTrackParams.headers = ["":"","":""]
+
+        chatController.eventTrackerHandler = EventTrackerHandler(baseTrackParams: baseTrackParams)
+        let trackEventData1 = TrackEventData()
+
+        trackEventData1.eventName = "KEY - First Query to Bot Sent";
+        trackEventData1.isActive = true
+        let trackEventData2 = TrackEventData()
+        trackEventData2.eventName = "LIVECHAT - In Queue"
+        trackEventData2.isActive = false
+
+        let trackingEventDataDict = [TrackKeys.0:trackEventData1, TrackKeys.1:trackEventData2]
+        
+//        chatController.eventTrackerHandler.events =  (trackingEventDataDict as NSDictionary).mutableCopy() as! NSMutableDictionary
+//        let eventParams = EventParams(trackKey: FIRST_QUERY_BOLD)
+//        eventParams.aiAccountName = "myAccount"
+//        eventParams.chatProvider = "myChatProvider"
+//        chatController.eventTrackerHandler.trackEvent(eventParams)
+//
+//         BOT event Tracking
+        chatController.eventTrackerHandler = BotEventTrackerHandler(baseTrackParams: baseTrackParams)
+        chatController.eventTrackerHandler.events =  (trackingEventDataDict as NSDictionary).mutableCopy() as! NSMutableDictionary
+        let botEventParams = BotEventParams(trackKey: TrackKeys.0 as String)
+        botEventParams.kbLanguage = "mobiledemo"
+        botEventParams.chatProvider = "europe"
+        botEventParams.from = "faq"
+        
+        chatController.eventTrackerHandler.trackEvent(botEventParams)
     }
 }
